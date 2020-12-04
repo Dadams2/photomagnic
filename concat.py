fc1e47434db681cfeb80c28e06413b5442953f7f
dadams2 <dbadams@iinet.net.au>
2020-12-04 21:37:43 +0800

    adding some better draft code

---
+++ concat.py
@@ -0,0 +1,29 @@
+import cv2 
+import numpy as np 
+
+im1=cv2.imread("images/images.jpg")
+im2=cv2.imread("images/images2.jpg")
+
+
+def hconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
+    h_min = min(im.shape[0] for im in im_list)
+    im_list_resize = [cv2.resize(im, (int(im.shape[1] * h_min / im.shape[0]), h_min), interpolation=interpolation)
+                      for im in im_list]
+    return cv2.hconcat(im_list_resize)
+
+def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
+    w_min = min(im.shape[1] for im in im_list)
+    im_list_resize = [cv2.resize(im, (w_min, int(im.shape[0] * w_min / im.shape[1])), interpolation=interpolation)
+                      for im in im_list]
+    return cv2.vconcat(im_list_resize)
+
+def concat_tile_resize(im_list_2d, interpolation=cv2.INTER_CUBIC):
+    im_list_v = [hconcat_resize_min(im_list_h, interpolation=cv2.INTER_CUBIC) for im_list_h in im_list_2d]
+    return vconcat_resize_min(im_list_v, interpolation=cv2.INTER_CUBIC)
+
+
+
+im_tile_resize = concat_tile_resize([[im1],
+                                     [im1, im2, im1, im2, im1],
+                                     [im1, im2, im1]])
+cv2.imwrite('out2.jpg', im_tile_resize)
